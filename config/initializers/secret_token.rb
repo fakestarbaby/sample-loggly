242e0ac0d583e74e57721dc7ae1d494aa30d6d9f
fakestarbaby <fakestarbaby@gmail.com>
2011-09-19 22:01:20 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleLoggly::Application.config.secret_token = 'ddccf97a0ade35376f8dd501d707c4f2309239247048bde05ea4f604aa401f753469476d850ff37f9bdb2e49a2fb517c29c344ef263ffcb21eec43d763334c30'
